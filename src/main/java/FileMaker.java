db1f1d7faa3004f30b7d11aec75f3f75125d91db
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-25 23:50:14 +0100

    working writeInFile method

--- src/main/java/FileMaker.java
+++ src/main/java/FileMaker.java
@@ -1,17 +1,35 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class FileMaker {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
+
+        createFile("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
+        writeInFile("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt", "Kurs \nJava \nLekcja6 \nPliki \nWyjątki \nPliki \nKoniec pliku" );
+
+    }
+    public static void createFile(String filePath){
         try {
-            File file = new File("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
-            if (file.createNewFile()){
+            File file = new File(filePath);
+            if (file.createNewFile()) {
                 System.out.println("File created: " + file.getName());
             }
         }catch (IOException e){
-            System.out.println("Error.");
+            System.out.println("An Error occured");
             e.printStackTrace();
-
         }
+    }
+    public static void writeInFile(String filePath, String text){
+        try {
+            Files.writeString(Paths.get(filePath), text);
+
+        } catch (IOException e){
+
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+
+
     }
 }
