667517710e90b64e4cf0fef291b61a59fbe0d6bc
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-28 22:40:12 +0100

    Working method for getting uppercase in a new file

--- src/main/java/ToUpperCaseMaker.java
+++ src/main/java/ToUpperCaseMaker.java
@@ -4,14 +4,14 @@ import java.nio.file.Path;
 
 public class ToUpperCaseMaker {
     public static void main(String[] args) {
-
+        getUpperCase("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt", "E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\output.txt");
     }
     public static void getUpperCase(String filePath, String newFilePath){
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
             String fileContent = Files.readString(Path.of(filePath));
-            System.out.println(fileContent);
-
+            String fileContentUpperCase = fileContent.toUpperCase();
+            File file = new File(newFilePath);
+            Files.writeString(Path.of(newFilePath), fileContentUpperCase);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
