a392c531d5cdec357c921846b26e3c4f7abef7b5
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-28 23:46:37 +0100

    Method for getting sqrt from number changed to return double

--- src/main/java/SquareRoot.java
+++ src/main/java/SquareRoot.java
@@ -1,10 +1,11 @@
 public class SquareRoot {
-    public static void getSquareRoot (double number){
+    public static double getSquareRoot (double number){
         if (number < 0){
             throw new IllegalArgumentException();
         }
         else {
             System.out.println(Math.sqrt(number));
         }
+        return Math.sqrt(number);
     }
 }
