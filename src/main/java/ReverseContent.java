27c2c83272c95633e41f7b2153778bffe75b42da
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-28 20:26:15 +0100

    Working method for reverse showing file content

--- src/main/java/ReverseContent.java
+++ src/main/java/ReverseContent.java
@@ -1,23 +1,24 @@
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReverseContent {
+    public static void main(String[] args) {
+        showReversedFileContent("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
+    }
 
-    public void showReversedFileContent(String filePath){
+    public static void showReversedFileContent(String filePath){
     try {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         String contentOfTheFile = Files.readString(Paths.get(filePath));
-        String reverse = "";
-
-
-
-
-    } catch (FileNotFoundException e) {
-        throw new RuntimeException(e);
+        int allLinesCount = (int) contentOfTheFile.lines().count();
+        while (allLinesCount >= 1){
+            String line = Files.readAllLines(Path.of(filePath)).get(allLinesCount - 1);
+            System.out.println(line);
+            allLinesCount--;
+        }
     } catch (IOException e) {
         throw new RuntimeException(e);
     }
