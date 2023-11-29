916ad273ecc30b05f37dc8b366e81fb98079ef30
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-11-29 21:15:37 +0100

    get and show methods

--- src/main/java/ReverseContent.java
+++ src/main/java/ReverseContent.java
@@ -7,20 +7,35 @@ import java.nio.file.Paths;
 
 public class ReverseContent {
     public static void main(String[] args) {
-        showReversedFileContent("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
+        getReversedFileContent("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
+        showReversedContent("E:\\PROGRAMOWANIE\\KURSY\\PROJEKTY\\Files-Exceptions\\src\\main\\resources\\data.txt");
     }
 
-    public static void showReversedFileContent(String filePath){
-    try {
-        String contentOfTheFile = Files.readString(Paths.get(filePath));
-        int allLinesCount = (int) contentOfTheFile.lines().count();
-        while (allLinesCount >= 1){
-            String line = Files.readAllLines(Path.of(filePath)).get(allLinesCount - 1);
-            System.out.println(line);
-            allLinesCount--;
+    public static void getReversedFileContent(String filePath){
+        try {
+            String contentOfTheFile = Files.readString(Paths.get(filePath));
+            int allLinesCount = (int) contentOfTheFile.lines().count();
+            while (allLinesCount >= 1){
+                String line = Files.readAllLines(Path.of(filePath)).get(allLinesCount - 1);
+                allLinesCount--;
+            }
         }
-    } catch (IOException e) {
+        catch (IOException e) {
         throw new RuntimeException(e);
+        }
     }
+    public static void showReversedContent(String filePath){
+        try {
+            String contentOfTheFile = Files.readString(Paths.get(filePath));
+            int allLinesCount = (int) contentOfTheFile.lines().count();
+            while (allLinesCount >= 1){
+                String line = Files.readAllLines(Path.of(filePath)).get(allLinesCount - 1);
+                System.out.println(line);
+                allLinesCount--;
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
