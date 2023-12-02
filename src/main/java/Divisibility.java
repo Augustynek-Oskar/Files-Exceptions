f066671fcb00b4a62da1c867d0f86b5d9ada922f
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-03 00:31:51 +0100

    new method

---
+++ src/main/java/Divisibility.java
@@ -0,0 +1,13 @@
+public class Divisibility {
+    public static void main(String[] args) {
+
+    }
+
+    public static void validateIfNumberIsDivisibleByThree(int[] value){
+        try {
+
+        }
+        catch ()
+
+    }
+}
