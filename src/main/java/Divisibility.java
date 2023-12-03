b89ffe7ede3d955fa6e7e388f32e4f6a7a805385
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-03 23:42:06 +0100

    working method for checking if number is divisible by three.

--- src/main/java/Divisibility.java
+++ src/main/java/Divisibility.java
@@ -1,11 +1,20 @@
+import java.util.Arrays;
+
 public class Divisibility {
     public static void main(String[] args) {
         int [] numbersArray = {1, 2 ,3 ,4 ,6};
-        validateIfNumberIsDivisibleByThree(numbersArray);
+        isNumberDivisibleByThree(numbersArray);
     }
 
-    public static void validateIfNumberIsDivisibleByThree(int[] value){
-
+    public static void isNumberDivisibleByThree(int[] values){
+        for (int number : values){
+            try {
+                if (number % 3 == 0) System.out.println(number + " jest podzielna przez 3!");
+                else throw new IllegalArgumentException();
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(number + " nie jest podzielne przez 3!");
+            }
+        }
     }
-
 }
