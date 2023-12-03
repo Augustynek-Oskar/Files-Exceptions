8b9a694bec8f832cb1ee6749c764402444d175aa
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-03 18:51:07 +0100

    checkIfPeselDataTypeIsCorrect now throws exception without catching it

--- src/main/java/PeselChecker.java
+++ src/main/java/PeselChecker.java
@@ -2,29 +2,21 @@ public class PeselChecker {
     public static void main(String[] args) throws IllegalLengthException {
 
         checkIfPeselLengthIsCorrect("12345678901");
-        getPeselDataTypeValidation("1236789011");
+        checkIfPeselDataTypeIsCorrect("12345678901");
 
     }
     public static void checkIfPeselLengthIsCorrect(String pesel) throws IllegalLengthException {
-
         if (pesel == null) throw new IllegalLengthException("Pesel cannot be null!");
 
         if (pesel.length() != 11) throw new IllegalLengthException("Incorrect length!");
 
         else System.out.println("Length is correct");
-
     }
-    public static void getPeselDataTypeValidation(String pesel) {
-        try {
-            if (pesel.matches(".*[a-z].*")){
-            throw new WrongTypeOfDataException("Illegal data type in your pesel!");
-            }
-            else System.out.println("Correct data type format!");
-        }catch (WrongTypeOfDataException e){
-            System.out.println(e.getMessage());
-        }
+    public static void checkIfPeselDataTypeIsCorrect(String pesel) {
+        if (pesel == null) throw new WrongTypeOfDataException("Pesel cannot be null!");
 
+        if (pesel.matches(".*[a-z].*")) throw new WrongTypeOfDataException("Illegal data type in your pesel!");
 
-
+        else System.out.println("Correct data type format!");
     }
 }
