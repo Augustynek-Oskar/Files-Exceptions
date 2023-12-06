66595578b1e1d511fdbca17d7e64f4ba5e70699f
Oskar Augustynek <oskar.augustynek@gmail.com>
2023-12-06 23:48:15 +0100

    refactor

--- src/main/java/Divisibility.java
+++ src/main/java/Divisibility.java
@@ -1,26 +1,25 @@
 import java.util.Arrays;
-
 public class Divisibility {
     public static void main(String[] args) {
         int [] numbersArray = {0, 1, 2 ,3 ,4 ,6};
-        isNumberDivisibleByThree(numbersArray);
+
+        areElementsDivisibleByThree(numbersArray);
+
     }
-
-    public static void isNumberDivisibleByThree(int[] values){
-        for (int number : values){
+    public static void areElementsDivisibleByThree(int[] values){
+        for (int number : values) {
             try {
-                if (number == 0) throw new ArithmeticException();
-
-                if (number % 3 == 0) System.out.println(number + " jest podzielna przez 3!");
-
-                else throw new IllegalArgumentException();
+                isDivisibleByThree(number);
             }
             catch (IllegalArgumentException e){
-                System.out.println(number + " nie jest podzielne przez 3!");
-            }
-            catch (ArithmeticException e){
-                System.out.println("Dzielenie przez zero jest zabronione!");
+                System.out.println(e.getMessage());
             }
         }
+    }
+    public static void isDivisibleByThree (int number){
+        if (number % 3 != 0){
+            throw new IllegalArgumentException(number + ": nie jest podzielne przez 3!");
+        }
+        else System.out.println(number + ": jest podzielne przez 3!");
     }
 }
